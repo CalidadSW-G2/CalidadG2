308ca7eec5e1df53f2435842bbd0c27fbfc699a3
Enrique <kikemarquezjimenez@gmail.com>
2020-04-15 12:42:02 +0200

    Update Features

--- app/src/androidTest/assets/features/randomColors.feature
+++ app/src/androidTest/assets/features/randomColors.feature
@@ -5,3 +5,33 @@ Feature: RandomColors
     Given squareIsRed
     When RandomButtonIsSelected
     Then SquareCanBeAnythingButRed
+
+  @randomColorL
+  Scenario: lChangesRandomly
+    Given lIsBlue
+    When RandomButtonIsSelected
+    Then lCanBeAnythingButBlue
+
+  @randomColorZ
+  Scenario: zChangesRandomly
+      Given zIsGreen
+      When RandomButtonIsSelected
+      Then zCanBeAnythingButGreen
+
+  @randomColorI
+  Scenario: iChangesRandomly
+    Given iIsYellow
+    When RandomButtonIsSelected
+    Then iCanBeAnythingButYellow
+
+  @randomColorT
+  Scenario: tChangesRandomly
+     Given tIsPink
+     When RandomButtonIsSelected
+     Then tCanBeAnythingButPink
+
+  @randomColorS
+  Scenario: sChangesRandomly
+    Given sIsOrange
+    When RandomButtonIsSelected
+    Then sCanBeAnythingButOrange
--- app/src/androidTest/java/com/example/tetris/test/RandomColorsSteps.java
+++ app/src/androidTest/java/com/example/tetris/test/RandomColorsSteps.java
@@ -10,6 +10,7 @@ import org.junit.Assert;
 
 public class RandomColorsSteps {
 
+    //Color range "Red" 0 , "Pink" 1 , "Light Blue" 2 , "Dark Blue" 3 , "Green" 4 , "Orange" 5 , "Yellow" 6 ,"Random" 7
 
     @Given("^squareIsRed$")
     public void squareisred() {
@@ -22,8 +23,66 @@ public class RandomColorsSteps {
     }
 
     @Then("^SquareCanBeAnythingButRed$")
-    public void redcanbeanythingbutred() {
+    public void squarecanbeanythingbutred() {
         Assert.assertTrue(Tablero.getColorCuadrado() != 7);
     }
+
+    //RandomColorL , blue
+    @Given("^lIsBlue$")
+    public void lisblue() {
+        Tablero.setColorCuadrado(7);
+    }
+
+    //RandonButtonIsSelected
+
+    @Then("^lCanBeAnythingButBlue$")
+    public void lcanbeanythingbutblue() {
+        Assert.assertTrue(Tablero.getColorCuadrado() != 7);
+    }
+
+    //RandomColorZ , Green
+    @Given("^zIsGreen$")
+    public void zisgreen() {
+        Tablero.setColorCuadrado(7);
+    }
+
+    @Then("^zCanBeAnythingButGreen$")
+    public void zcanbeanythingbutgreen() {
+        Assert.assertTrue(Tablero.getColorCuadrado() != 7);
+    }
+
+    //RandomColorI , Yellow
+    @Given("^iIsYellow$")
+    public void iisyellow() {
+        Tablero.setColorCuadrado(7);
+    }
+
+    @Then("^iCanBeAnythingButYellow$")
+    public void icanbeanythingbutyellow() {
+        Assert.assertTrue(Tablero.getColorCuadrado() != 7);
+    }
+
+    //RandomColorT , Pink
+    @Given("^tIsPink$")
+    public void tisPink() {
+        Tablero.setColorCuadrado(7);
+    }
+
+    @Then("^tCanBeAnythingButPink$")
+    public void tcanbeanythingbutpink() {
+        Assert.assertTrue(Tablero.getColorCuadrado() != 7);
+    }
+
+    //RandomColorS , Orange
+    @Given("^sIsOrange$")
+    public void sisOrange() {
+        Tablero.setColorCuadrado(7);
+    }
+
+    @Then("^sCanBeAnythingButOrange$")
+    public void scanbeanythingbutorange() {
+        Assert.assertTrue(Tablero.getColorCuadrado() != 7);
+    }
+
 
 }
