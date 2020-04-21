5825abee950672c1bb558a2602f6c56de9a373ed
Diego <diegojaraoro@gmail.com>
2020-04-21 13:31:01 +0200

    Added wait for simulator script

---
+++ android-wait-for-emulator.sh
@@ -0,0 +1,21 @@
+set +e
+
+bootanim=""
+failcounter=0
+timeout_in_sec=600
+
+until [[ "$bootanim" =~ "stopped" ]]; do
+  bootanim=`adb -e shell getprop init.svc.bootanim 2>&1 &`
+  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
+    || "$bootanim" =~ "running" || "$bootanim" =~  "error: no emulators found" ]]; then
+    let "failcounter += 1"
+    echo "Waiting for emulator to start: $failcounter of $timeout_in_sec : status: $bootanim"
+    if [[ $failcounter -ge timeout_in_sec ]]; then
+      echo "Timeout ($timeout_in_sec seconds) reached; failed to start emulator"
+      exit 1
+    fi
+  fi
+  sleep 1
+done
+
+echo "Emulator is ready"
